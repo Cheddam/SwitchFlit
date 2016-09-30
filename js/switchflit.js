cc18be4f1a268a607e3913eebaba0027ccb37fbc
Garion Herman <garion@silverstripe.com>
2016-09-30 20:08:24 +1300

    Update JS to conform to AirBnB linting rules.

--- js/switchflit.js
+++ js/switchflit.js
@@ -1,60 +1,59 @@
-require('es6-promise').polyfill();
-
-var Vue = require('vue')
-var Fuse = require('fuse.js')
+const Vue = require('vue');
+const Fuse = require('fuse.js');
 
 window.switchflit = new Vue({
-	el: '#switchflit',
+  el: '#switchflit',
 
-	props: ['dataobject'],
+  props: ['dataobject'],
 
-	data: {
-		visible: false,
-		records: [],
-		fuse: null,
-		query: ''
-	},
+  data: {
+    visible: false,
+    records: [],
+    fuse: null,
+    query: '',
+  },
 
-	computed: {
-		filteredRecords() {
-			if (!this.fuse) return []
+  computed: {
+    filteredRecords() {
+      if (!this.fuse) return [];
 
-			this.fuse.set(this.records)
+      this.fuse.set(this.records);
 
-			return this.fuse.search(this.query)
-		}
-	},
+      return this.fuse.search(this.query);
+    },
+  },
 
-	methods: {
-		show() { this.visible = true; window.setTimeout(() => { this.$el.querySelector('input').focus() }, 0) },
-		hide() { this.visible = false; this.query = '' },
+  methods: {
+    show() { this.visible = true; window.setTimeout(() => { this.$el.querySelector('input').focus(); }, 0); },
+    hide() { this.visible = false; this.query = ''; },
 
-		openResult() {
-			if (this.filteredRecords.length < 1) return true
+    openResult() {
+      if (this.filteredRecords.length < 1) return true;
 
-			window.location = this.filteredRecords[0].link
-		}
-	},
+      window.location = this.filteredRecords[0].link;
+      return false;
+    },
+  },
 
-	created() {
-		fetch(`/switchflit/${this.dataobject}/records`).then((response) => {
-			return response.json()
-		}).then((records) => {
-			this.records = records
+  created() {
+    fetch(`/switchflit/${this.dataobject}/records`)
+      .then(response => response.json())
+      .then((records) => {
+        this.records = records;
 
-			this.fuse = new Fuse(this.records, { keys: ['title'] })
-		})
-	},
+        this.fuse = new Fuse(this.records, { keys: ['title'] });
+      });
+  },
 
-	ready() {
-		this.$el.style.visibility = 'visible'
-	}
-})
+  ready() {
+    this.$el.style.visibility = 'visible';
+  },
+});
 
 document.addEventListener('keydown', (event) => {
-	if (event.keyCode == 27) {
-		window.switchflit.hide()
-	} else if ((navigator.platform == "MacIntel" && event.metaKey && event.keyCode == 75) || (event.ctrlKey && event.keyCode == 75)) {
-		window.switchflit.show()
-	}
-})
+  if (event.keyCode === 27) {
+    window.switchflit.hide();
+  } else if ((navigator.platform === 'MacIntel' && event.metaKey && event.keyCode === 75) || (event.ctrlKey && event.keyCode === 75)) {
+    window.switchflit.show();
+  }
+});
