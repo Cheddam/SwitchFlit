537b048763fb64536802cdd6a94d723cd9f11a9a
Garion Herman <garion@silverstripe.com>
2016-10-01 05:52:35 +1300

    Add CustomQuery interface and handling (fixes #4).

--- code/SwitchFlitable.php
+++ code/SwitchFlitable.php
@@ -4,7 +4,13 @@ namespace SwitchFlit;
 
 interface SwitchFlitable
 {
+    /**
+     * @return string The title to use in SwitchFlit for this DataObject
+     */
 	public function SwitchFlitTitle();
 
+    /**
+     * @return string The link to use in SwitchFlit for this DataObject
+     */
 	public function SwitchFlitLink();
 }
